60a0a26bb83f8d75c399ed6f8b6c370baa09e821
WuYiChenhml <1055200502@qq.com>
2017-07-15 15:28:18 +0800

    Update main-spec.js

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,4 +1,4 @@
-describe('pos', function () {
+describe('practice-1-3', function () {
     var allItems;
     var inputs;
 
@@ -18,19 +18,17 @@ describe('pos', function () {
 
     it('should print correct text', function () {
 
-        spyOn(console, 'log');
-
         printInventory(inputs);
 
         var expectText =
-            '***<没钱赚商店>购物清单***\n' +
-            '名称：可口可乐，数量：5瓶，单价：3.00(元)，小计：15.00(元)\n' +
-            '名称：雪碧，数量：2瓶，单价：3.00(元)，小计：6.00(元)\n' +
-            '名称：电池，数量：1个，单价：2.00(元)，小计：2.00(元)\n' +
-            '----------------------\n' +
-            '总计：23.00(元)\n' +
-            '**********************';
+                '***<没钱赚商店>购物清单***\n' +
+                '名称:可口可乐,数量:5瓶,单价:3.00(元),小计:15.00(元)\n' +
+                '名称:雪碧,数量:2瓶,单价:3.00(元),小计:6.00(元)\n' +
+                '名称:电池,数量:1个,单价:2.00(元),小计:2.00(元)\n' +
+                '----------------------\n' +
+                '总计:23.00(元)\n' +
+                '**********************';
 
-          expect(console.log).toHaveBeenCalledWith(expectText);
+          expect(logs[0]).toBe(expectText);
     });
 });
