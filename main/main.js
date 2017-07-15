44f6a14131d464bc8d1de7c85b4baf9b2e077f46
WuYiChenhml <1055200502@qq.com>
2017-07-15 15:28:43 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,52 +1,62 @@
-//TODO: Please write code in this file.
+//TODO: 请补完下面的函数以完成需求.
 function printInventory(inputs) {
-    var allItemInfo = countProduct(inputs);
-    printResult(allItemInfo);
-}
+        var allItemInfo = countProduct(inputs); 
+     printResult(allItemInfo); 
+ } 
+ 
+ 
+ function printResult(allItemInfo) { 
+     var sum = 0; 
+     var resultString = '***<没钱赚商店>购物清单***'; 
+ 
+ 
+     for (var i = 0; i < allItemInfo.length; i++) { 
+         if (allItemInfo[i].hasOwnProperty('count')) { 
+             sum += allItemInfo[i].getCost(); 
+             resultString += '\n' + getItemMessage(allItemInfo[i]); 
+         } 
+     } 
+ 
+ 
+     resultString += '\n----------------------\n总计:' + sum.toFixed(2) + '(元)'; 
+     resultString += '\n**********************'; 
+ 
+ 
+     console.log(resultString); 
+ } 
+ 
+ 
+ function countProduct(inputs) { 
+     var allItemInfo = loadAllItems(); 
+ 
+ 
+     for (var i = 0; i < inputs.length; i++) { 
+         var oneItemInfo = findMatchItemInfo(allItemInfo, inputs[i]); 
+         oneItemInfo.count = (oneItemInfo.hasOwnProperty('count')) ? oneItemInfo.count + 1 : 1; 
+         oneItemInfo.getCost = function() { 
+         	return this.price * this.count; 
+         } 
+     } 
+ 
+ 
+     return allItemInfo; 
+ } 
+ 
+ 
+ function findMatchItemInfo(allItemInfo, barcode) { 
+     for (var i = 0; i < allItemInfo.length; i++) { 
+         if (allItemInfo[i].barcode == barcode) { 
+             return allItemInfo[i]; 
+         } 
+     } 
+     return null; 
+ } 
+ 
+ 
+ function getItemMessage(item) { 
+     return '名称:' + item.name + 
+         ',数量:' + item.count + item.unit + 
+         ',单价:' + item.price.toFixed(2) + 
+         '(元),小计:' + item.getCost().toFixed(2) + '(元)'; 
+} 
 
-function printResult(allItemInfo) {
-    var sum = 0;
-    var resultString = '***<没钱赚商店>购物清单***';
-
-    for (var i = 0; i < allItemInfo.length; i++) {
-        if (allItemInfo[i].hasOwnProperty('count')) {
-            sum += allItemInfo[i].getCost();
-            resultString += '\n' + getItemMessage(allItemInfo[i]);
-        }
-    }
-
-    resultString += '\n----------------------\n总计：' + sum.toFixed(2) + '(元)';
-    resultString += '\n**********************';
-
-    console.log(resultString);
-}
-
-function countProduct(inputs) {
-    var allItemInfo = loadAllItems();
-
-    for (var i = 0; i < inputs.length; i++) {
-        var oneItemInfo = findMatchItemInfo(allItemInfo, inputs[i]);
-        oneItemInfo.count = (oneItemInfo.hasOwnProperty('count')) ? oneItemInfo.count + 1 : 1;
-        oneItemInfo.getCost = function() {
-        	return this.price * this.count;
-        }
-    }
-
-    return allItemInfo;
-}
-
-function findMatchItemInfo(allItemInfo, barcode) {
-    for (var i = 0; i < allItemInfo.length; i++) {
-        if (allItemInfo[i].barcode == barcode) {
-            return allItemInfo[i];
-        }
-    }
-    return null;
-}
-
-function getItemMessage(item) {
-    return '名称：' + item.name +
-        '，数量：' + item.count + item.unit +
-        '，单价：' + item.price.toFixed(2) +
-        '(元)，小计：' + item.getCost().toFixed(2) + '(元)';
-}
